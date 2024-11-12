418cf297198aec35381219d38a8ba48ea452f312
Andrew Fan <andrew.fan@cenovus.com>
2024-11-12 13:37:08 -0700

    chore: remove ignored files from git

--- rankerapi/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RankerapiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "rankerapi"
-
-    def ready(self):
-        import rankerapi.signals
